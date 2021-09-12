74b87e22ee7c2a2751bc7ce0ac51d3470f249ab8
hanasai95 <1220hanasai@gmail.com>
2021-09-12 16:29:04 -0400

    NavBar and introCard

---
+++ components/Home/IntroCard/index.js
@@ -0,0 +1,11 @@
+import { Typography } from "@material-ui/core";
+
+export default function IntroCard() {
+    return <div>
+        <Typography>
+            Hello, My name<br />
+            is Hana Cai
+        </Typography>
+    </div>
+}
+
